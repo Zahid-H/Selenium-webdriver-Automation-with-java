bea38dd4b0e0e6d49fc2d61a36bcd6c4065cdace
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-05-10 14:52:51 +0600

    github repositoray automaion code written

---
+++ Java Selenium/seleniumwithjava/src/main/java/com/sjava/githubAutomaion.java
@@ -0,0 +1,16 @@
+package com.sjava;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class githubAutomaion {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://github.com/Zahid-H/Selenium-webdriver-Automation-with-java");
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
